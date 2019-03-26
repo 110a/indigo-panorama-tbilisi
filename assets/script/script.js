31a5087ce400fe5eb866788afc2d78260bd0ed51
110a <a.asatiani@gipa.ge>
2019-03-27 01:28:57 +0400

    add makhos work to repository

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+let wcont = document.querySelector('.welcome-cont');
+document.querySelector('.wc-play').addEventListener('click',function () {
+    audio.play();
+    playPauseButton.classList.add('paused');
+    document.querySelector('.wc-play').classList.add('out');
+    document.querySelector('.wc-title').classList.add('out');
+    bottom = document.querySelector('.wc-text').classList.add('out');
+    document.querySelector('.main-content').style.opacity = '1';
+    setTimeout(function () {
+        wcont.style.display = "none";
+    },900)
+});
