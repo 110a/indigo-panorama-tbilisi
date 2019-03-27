2d92125169376199b0f9bde8a092dfa9fd6c8047
Makho Gelashvili <maxogelashvili96@gmail.com>
2019-03-27 15:14:11 +0400

    sound button bug solved

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,8 @@ document.querySelector('.wc-play').addEventListener('click',function () {
     document.querySelector('.wc-play').classList.add('out');
     document.querySelector('.wc-title').classList.add('out');
     bottom = document.querySelector('.wc-text').classList.add('out');
-    document.querySelector('.main-content').style.opacity = '1';
+    Object.assign(document.querySelector('.main-content').style,{opacity:"1", position: 'relative', height: "100%"});
+    Object.assign(document.querySelector('.sound-timing').style,{zIndex:'10'});
     setTimeout(function () {
         wcont.style.display = "none";
     },900)
