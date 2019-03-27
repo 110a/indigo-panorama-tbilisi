3478005292dc927fe4e8378a4d9afcb9388fcd6e
Makho Gelashvili <maxogelashvili96@gmail.com>
2019-03-27 20:02:44 +0400

    bugs solved

--- assets/script/textImageChanger.js
+++ assets/script/textImageChanger.js
@@ -1,11 +1,11 @@
 const textImages = [
-    {appearTime:0, title:"2015 წელი <br> 8 სექტემბერი", subtitle: "წ<br>ე<br>რ<br>ი<br>ლ<br>ი<br>: ", text:"თბილისი მდინარის ხეობის გასწვრივ გაშენებული, მთებს შორის მოქცეული გამორჩეული სილამაზის ქალაქია. ამ ჰარმონიას სწორედ მისი ბუნებრივი გარემო ქმნის. თბილისის ქედები 1975 წლიდანაა დაცული, რადგან ისინი ქალაქის ხასიათისა და მისი კულტურული მნიშვნელობის განუყოფელი ნაწილია"},
-    {appearTime:20, title:"14 საერთაშორისო ორგანიზაციის", subtitle:"თ<br>ხ<br>ო<br>ვ<br>ნ<br>ა<br>: ", text:"14 საერთაშორისო ორგანიზაცია საქართველოს მთავრობას სთხოვდა არ დაეშვათ თბილისის ისტორიული ლანდშაფტის გულში „პანორამა თბილისის“ აშენება. "},
+    {appearTime:0, title:"2015 წელი <br> 8 სექტემბერი", subtitle: "წ<br>ე<br>რ<br>ი<br>ლ<br>ი<br>: ", text:"თბილისი მდინარის ხეობის გასწვრივ გაშენებული, მთებს შორის მოქცეული გამორჩეული სილამაზის ქალაქია. ამ ჰარმონიას სწორედ მისი ბუნებრივი გარემო ქმნის. თბილისის ქედები 1975 წლიდანაა დაცული, რადგან ისინი ქალაქის ხასიათისა და მისი კულტურული მნიშვნელობის განუყოფელი ნაწილია."},
+    {appearTime:20, title:"14 საერთაშორისო <br> ორგანიზაციის", subtitle:"თ<br>ხ<br>ო<br>ვ<br>ნ<br>ა<br>: ", text:"14 საერთაშორისო ორგანიზაცია საქართველოს მთავრობას სთხოვდა არ დაეშვათ თბილისის ისტორიული ლანდშაფტის გულში „პანორამა თბილისის“ აშენება. "},
     {appearTime:33, title:"თბილისი", subtitle:"", text:"თბილისი მდინარის ხეობის გასწვრივ გაშენებული, მთებს შორის მოქცეული ქალაქია."},
     {appearTime:42, title:"ჰარმონია", subtitle:"", text:"ჰარმონიას თბილისის ბუნებრივი გარემო ქმნის. "},
     {appearTime:50, title:"თბილისი", subtitle:"", text:"თბილისის ქედები მისი ხასიათის განუყოფელი ნაწილია."},
     {appearTime:60, title:"1985 წელი", subtitle:"", text:"1985 წლიდან, ქალაქის ამ ისტორიულ ნაწილს სამი ზონა იცავს."},
-    {appearTime:84, title:"ისტორიული განაშენიანების დაცვის ზონა", subtitle:"", text:"ისტორიული განაშენიანების დაცვის ზონაში დღესაც დაუშვებელია მშენებლობა."},
+    {appearTime:84, title:"ისტორიული განაშენიანების<br> დაცვის ზონა", subtitle:"", text:"ისტორიული განაშენიანების დაცვის ზონაში დღესაც დაუშვებელია მშენებლობა."},
     {appearTime:95, title:"1985 წელი", subtitle:"კ<br>ა<br>ნ<br>ო<br>ნ<br>ი: ", text:"1985 წლიდან კანონი იცავს თბილისის ბუნებრივს რელიეფს."},
     {appearTime:122, title:"1985 წელი", subtitle:"დ<br>ა<br>დ<br>გ<br>ე<br>ნ<br>ი<br>ლ<br>ე<br>ბ<br>ა:", text:"1985 წლის დადგენილებას დღემდე არ დაუკარგავს ძალა."},
     {appearTime:135, title:"სოლოლაკის ქედი", subtitle:"ნ<br>ე<br>ბ<br>ა<br>რ<br>თ<br>ვ<br>ი<br>ს<br>გ<br>ა<br>რ<br>ე<br>შ<br>ე<br>:", text:"სოლოლაკის ქედზე, ბიძინა ივანიშვილის საცხოვრებელი სახლისა და ბიზნეს კომპლექსის მშენებლობა ნებართვის გარეშე დაიწყო."},
@@ -16,28 +16,29 @@ const textImages = [
     {appearTime:220, title:"ექსკლუზიურად", subtitle:"ა<br>ხ<br>ა<br>ლ<br>ი<br>გ<br>ე<br>ნ<br>-<br>გ<br>ე<br>გ<br>მ<br>ა<br>:", text:"ექსკლუზიურად „პანორამა თბილისისთვის“ ახალი გენ-გეგმა მომზადდა, სადაც მშენებლობისთვის  არეალში ყველა დამცავი ზონა გამქრალი იყო. "},
     {appearTime:240, title:"თბილისი", subtitle:"", text:"თბილისის უნიკალური რელიეფი შეუქცევადად შეიცვალა."},
     {appearTime:260, title:"ახალი გენ-გეგმა", subtitle:"", text:"ახალ გენ-გეგმაში ისტორიული ლანდშაფტის დაცვის ზონა ისევ ბრუნდება."},
-    {appearTime:270, title:"გერგეთის ქუჩა N 1", subtitle:"პ<br>ი<br>რ<br>ო<br>ბ<br>ა<br>:", text:"თბილისის მერი პირობას დებს რომ შეჩერდება სასტუმროს მშენებლობა გერგეთის ქუჩა N 1-შიც. "},
+    {appearTime:270, title:"გერგეთის ქუჩა <br> N 1", subtitle:"პ<br>ი<br>რ<br>ო<br>ბ<br>ა<br>:", text:"თბილისის მერი პირობას დებს რომ შეჩერდება სასტუმროს მშენებლობა გერგეთის ქუჩა N 1-შიც. "},
     {appearTime:290, title:"„პანორამა თბილისი“", subtitle:"უ<br>ხ<br>ე<br>შ<br>ა<br>დ", text:"„პანორამა თბილისი“ უხეშად იჭრება თბილისის ისტორიულ ლანდშაფტში. "},
     {appearTime:301, title:"მაგრამ", subtitle:"მ<br>ა<br>გ<br>რ<br>ა<br>მ<br>:", text:"მაგრამ ის მაინც აშენდება."}
 ];
 
 let title = document.querySelector('h1.headings');
 let subtitle = document.querySelector("h2.headings");
-let text = document.querySelector(".quotes");
+let text = document.querySelector(".inner-text");
 let current = 0;
-
+let qbefore = document.querySelector('.q-sign.before');
+let qafter = document.querySelector('.q-sign.after');
 let textImageHandler = () =>{
     if (audio.currentTime >= parseInt(textImages[current].appearTime) ){
-        title.style.opacity = "0";
-        subtitle.style.opacity = "0";
-        text.style.opacity = "0";
         title.innerHTML = textImages[current].title;
         subtitle.innerHTML = textImages[current].subtitle;
         text.innerHTML = textImages[current].text;
-        title.style.opacity = "1";
-        subtitle.style.opacity = "1";
-        text.style.opacity = "1";
         current++;
+    }
+    if(current > 1){
+        qbefore.style.visibility="hidden";
+        qafter.style.visibility="hidden";
+        text.style.fontFamily = "\"GL Satitulo\", sans-serif";
+        text.style.fontSize = "26px";
     }
 };
 
